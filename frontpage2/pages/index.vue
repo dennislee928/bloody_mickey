da480cd7e252afc8e0f6ca561decd67792e40550
e.an <55692615+dennislee928@users.noreply.github.com>
2024-05-23 16:55:07 +0800

    Update index.vue

--- frontpage2/pages/index.vue
+++ frontpage2/pages/index.vue
@@ -1,6 +1,18 @@
 <template>
   <Navbar />
   <IndexCard />
+  <div class="marquee-container">
+  <div class="marquee-content">
+    "Gosh, I just tried to make a sandwich, but I put the peanut butter on my shoe instead! Oh boy!"
+
+"Well, would ya look at that! I just found a rubber ducky in my cereal! Haha, how did that get there?"
+
+"Hot dog! I accidentally used shampoo as toothpaste this morning! My breath smells like strawberries!"
+
+"Aw, shucks! I was trying to bake a cake, but I ended up with a pizza! How'd that happen?"
+
+"Gee whiz! I thought I was planting flowers, but it turns out I was watering my socks! Haha!" </div>
+</div>
 </template>
 
 <script setup></script>
@@ -24,5 +36,56 @@
 }
 .second-title {
   font-size: 60px;
+}
+/**marquee part */
+
+.marquee-container {
+  width: 100%;
+  overflow: hidden;
+  display: flex;
+  font-size: x-large;
+}
+
+.marquee-content {
+  display: inline-block;
+  white-space: nowrap;
+  animation: marquee 50s linear infinite;
+}
+
+.marquee-content2 {
+  display: inline-block;
+  white-space: nowrap;
+  animation: marquee2 50s linear infinite;
+}
+.marquee-content3 {
+  display: inline-block;
+  white-space: nowrap;
+  animation: marquee2 50s linear infinite;
+}
+
+@keyframes marquee {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
+
+@keyframes marquee2 {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
+@keyframes marquee3 {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
 }
 </style>
